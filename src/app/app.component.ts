fbea801e2d3032ea4d5a7e2072a3e7ea78be492b
Nizami Sevindi <nsevindi87@gmail.com>
2024-05-17 23:07:32 +0200

    paginate pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  itemsPerPage = 2;
+  currentPage = 1;
   data = [
     {firstName: 'Nizami', lastName: 'Sevindi', email: 'nsevindi@exm.com'},
     {firstName: 'Peter', lastName: 'Example', email: 'abc@exm.com'},
@@ -16,4 +18,15 @@ export class AppComponent {
     {firstName: 'Handi', lastName: 'Hanre', email: 'asd@exm.com'},
     {firstName: 'Kundu', lastName: 'Ders', email: 'jggj@exm.com'},
   ]
+
+  get PaginatedData() {
+    const start = (this.currentPage -1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+
+    return this.data.slice(start, end)
+  }
+
+  changePage(page:number){
+    this.currentPage = page;
+  }
 }
--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -9,15 +9,30 @@ export class PaginationComponent {
   @Input() totalItems:any;
   @Input() currentPage:any;
   @Input() itemsPerpage:any;
+  @Output() onClick: EventEmitter<number> = new EventEmitter();
   totalPages = 0;
+  pages:number[]=[]
 
   constructor(){
 
   }
 
   ngOnInit():void{
+    this.createPaginate()
+
+  }
+
+  createPaginate = ()=>{
     if(this.totalItems){
       this.totalPages = Math.ceil(this.totalItems/this.itemsPerpage)
+      this.pages = Array.from({length: this.totalPages}, (_,i)=>i + 1)
   }
-}
+  }
+
+  pageClicked(page:number){
+    if(page>this.totalPages) return;
+    this.onClick.emit(page)
+  }
+
+
 }
