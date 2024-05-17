0e102c6941d90878e8258b7f651d61d3d32813d1
Nizami Sevindi <nsevindi87@gmail.com>
2024-05-17 22:18:24 +0200

    bootstrap and data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spacex-app';
+  data = [
+    {firstName: 'Nizami', lastName: 'Sevindi', email: 'nsevindi@exm.com'},
+    {firstName: 'Peter', lastName: 'Example', email: 'abc@exm.com'},
+    {firstName: 'Jacson', lastName: 'Serse', email: 'def@exm.com'},
+    {firstName: 'Mark', lastName: 'Derte', email: 'ghr@exm.com'},
+    {firstName: 'Selin', lastName: 'Karet', email: 'tge@exm.com'},
+    {firstName: 'Merva', lastName: 'Merrt', email: 'sdc@exm.com'},
+    {firstName: 'Handi', lastName: 'Hanre', email: 'asd@exm.com'},
+    {firstName: 'Kundu', lastName: 'Ders', email: 'jggj@exm.com'},
+  ]
 }
