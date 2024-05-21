27676f56f9ba8939c688a80603fe746d3620ae99
Nizami Sevindi <nsevindi87@gmail.com>
2024-05-21 22:20:32 +0200

    updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { FooterComponent } from './footer/footer.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent, PaginationComponent,FooterComponent],
+      imports: [HttpClientTestingModule, FormsModule]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'spacex-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('spacex-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+  it('should have a title "SpaceX Searching App"', () => {
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('spacex-app app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('SpaceX Searching App');
+  });
+
+  it('should fetch data from the API', () => {
+    spyOn(component, 'getData').and.callThrough();
+    component.ngOnInit();
+    expect(component.getData).toHaveBeenCalled();
+  });
+
+  
+
+  it('should change items per page', () => {
+    component.onChangeItemsPerPage({ target: { value: '3' } });
+    expect(component.itemsPerPage).toBe(3);
+  });
+
+  it('should update search term', () => {
+    const inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+    inputElement.value = 'Test';
+    inputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(component.searchTerm).toBe('Test');
   });
 });
--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -18,4 +18,19 @@ describe('FooterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should have background color dark and text color primary', () => {
+    fixture.detectChanges();
+    const footerElement = fixture.nativeElement as HTMLElement;
+    const footerDiv = footerElement.querySelector('.text-center');
+    expect(footerDiv).toBeTruthy();
+    expect(footerDiv?.classList).toContain('bg-dark');
+    expect(footerDiv?.classList).toContain('text-primary');
+  });
+
+  it('should have correct content', () => {
+    const footerElement: HTMLElement = fixture.nativeElement.querySelector('footer');
+    const copyrightText = '© 22 March 2002 | SIX | Nizami Sevindi';
+    expect(footerElement.textContent).toContain(copyrightText);
+  });
 });
